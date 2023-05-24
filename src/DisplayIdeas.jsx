218965fbd0f126b13ab77ceae56b528407aa99f2
ARod88 <alma.rodriguez72@yahoo.com>
2023-05-23 19:35:00 -0700

    lesson

---
+++ src/DisplayIdeas.jsx
@@ -0,0 +1,11 @@
+function DisplayIdeas (props) {
+    return (
+        <div>
+            <ul>
+                {props.ideas.map((idea, i) => <li key={i}>{idea}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default DisplayIdeas
---
+++ src/IdeasForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function IdeasForm (props) {
+    let [newIdea, setNewIdea] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addIdeas(newIdea)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={(e) => setNewIdea(e.target.value)} />
+            <button type="submit">Submit your awesome idea!</button>
+        </form>
+    )
+}
+
+export default IdeasForm
